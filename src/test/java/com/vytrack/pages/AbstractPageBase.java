0da31e4b6b6a41122eb01cf0300b69cae68d9a94
*alison9797* <*aliszak9727@gmail.com*>
2020-05-12 09:16:42 -0400

    first commit

---
+++ src/test/java/com/vytrack/pages/AbstractPageBase.java
@@ -0,0 +1,64 @@
+package com.vytrack.pages;
+
+
+import com.vytrack.utilities.BrowserUtilities;
+import com.vytrack.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+/***
+ * this class will be extended by page classes
+ * Any common webelements/locators can be stored here
+ * Since navigation menu doesn't  belong to particular page
+ * We can not really create a dedicated page
+ * elements from that menu
+ */
+public abstract class AbstractPageBase {
+
+    protected WebDriver driver = Driver.getDriver();
+    protected WebDriverWait wait = new WebDriverWait(driver, 15);
+
+    @FindBy(css= "#user-menu > a")
+    protected WebElement currentUser;
+    public AbstractPageBase(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    public String getCurrentUserName(){
+        BrowserUtilities.waitForPageToLoad(10);
+        wait.until(ExpectedConditions.visibilityOf(currentUser));
+        return currentUser.getText().trim();
+    }
+
+    /***
+     * Method for vytrack navigation. Provide tab and module name to navigate
+     *
+     * @param tabName like Dashboards, Fleet or Customers
+     * @param moduleNAme like Vehicles, Vehicles Odometer and Vehicle Costs
+     */
+
+    public void navigateTo(String tabName, String moduleNAme){
+        String tabNameXpath = "//span[@class='title title-level-1' and contains(text(), '"+tabName+"')]";
+        String moduleNameXpath = "//span[@class='title title-level-2' and text()='"+moduleNAme+"']";
+        Actions actions = new Actions(driver);
+
+        WebElement tabElement = driver.findElement(By.xpath(tabNameXpath));
+        WebElement moduleElement = driver.findElement(By.xpath(moduleNameXpath));
+
+        BrowserUtilities.wait(4);
+
+        actions.moveToElement(tabElement).pause(2000).
+                click(moduleElement).build().perform();
+
+        //increase this wait time if still failing
+        BrowserUtilities.wait(7);
+
+
+    }
+}
---
+++ src/test/java/com/vytrack/pages/LoginPage.java
@@ -0,0 +1,74 @@
+package com.vytrack.pages;
+
+//make this Driver comes from utilities
+import com.automation.tests.utilities.BrowserUtilities;
+import com.automation.tests.utilities.ConfigurationReader;
+import com.automation.tests.utilities.Driver;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    //these two are instant variables
+    @FindBy(id="prependedInput")
+    private WebElement username;
+
+    @FindBy(id = "prependedInput2")
+    private WebElement password;
+
+    @FindBy(id = "_submit")
+  private WebElement login;
+
+    @FindBy(linkText = "Forgot your password?")
+    private WebElement forgotPassword;
+
+    @FindBy(css = "[class='alert alert-error']")
+    private WebElement warningMessage;
+
+
+
+    //THIS IS CONSTRUCTOR
+    public LoginPage(){
+        //this method helps us to able it
+        //to connect out webdriver, page class and PageFactory to find elements
+        //PageFactory - used to use @FindBy annotations
+        //PageFactory - helps us to find elements easier
+
+        //this line will be always the same
+        PageFactory.initElements(Driver.getDriver(), this);
+
+    }
+
+    //method that returns a text of warning message webelement
+    // Call it in the test class
+    public String getWarningMessageText(){
+        return warningMessage.getText();
+    }
+
+    /***
+     * Method to login , version #1
+     * @param usernameValue
+     * @param passwordValue
+     * log in as a specific user
+     */
+    public void login(String usernameValue , String passwordValue){
+        username.sendKeys(usernameValue);
+        password.sendKeys(passwordValue, Keys.ENTER);
+        BrowserUtilities.wait(3);
+    }
+
+    /***
+     * Method to login
+     * Credentials will be retrieved from configuration.properties file
+     */
+    public  void login(){
+        username.sendKeys(ConfigurationReader.getProperty("store_manager"));
+        password.sendKeys(ConfigurationReader.getProperty("password"), Keys.ENTER);
+        BrowserUtilities.wait(3);
+    }
+    }
+
+
+
